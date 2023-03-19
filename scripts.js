680d719a3113893f09148704c604ee55e5f3cdd9
Ari-Markus Bjorkbacka <ari.markus.bjorkbacka@gmail.com>
2023-03-19 15:38:17 +0200

    comment changed

--- scripts.js
+++ scripts.js
@@ -45,7 +45,7 @@ for (i = 0; i < close.length; i++) {
         div.style.display = "none";
     }
 }
-// this should create checked mark when item is clicked. 
+// this creates checked mark when item is clicked. 
 var list = document.getElementById('doUL');
 list.addEventListener('click', function(e) {
     var li = e.target;
